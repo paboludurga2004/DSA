aed4e454d44c0d2ecaac015ffc472cc9b9534d8b
PABOLU DURGA PRASAD <paboludurga2004@gmail.com>
2025-12-27 22:35:07 +0530

    Add BasicsOfArrays class to print array elements

---
+++ Day-01/BasicsOfArrays.java
@@ -0,0 +1,8 @@
+public class BasicsOfArrays {
+    public static void main(String[] args) {
+        int[] arr={2,3,4,1,6};
+        for (int i=0;i<5;i++){
+            System.out.println(arr[i]+" ");
+        }
+    }
+}
