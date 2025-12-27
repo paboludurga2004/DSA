e573f663c1acb89b6c19db9eeaa0942d827c2661
PABOLU DURGA PRASAD <paboludurga2004@gmail.com>
2025-12-27 22:34:22 +0530

    Create function.java

---
+++ Day-01/function.java
@@ -0,0 +1,14 @@
+public class function {
+    static void funToPrint1Ton(int n){
+        int i=1;
+        while (i<=n){
+            System.out.println(i);
+            i++;
+        }
+    }
+    public static void main(String[] args) {
+        
+        funToPrint1Ton(10);
+
+    }
+}
