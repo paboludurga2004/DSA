e187e5e91c1097d3298115d9a39fab28ec8c719c
PABOLU DURGA PRASAD <paboludurga2004@gmail.com>
2025-12-27 22:28:04 +0530

    Add datatypes.java with basic data type examples

---
+++ Day-01/datatypes.java
@@ -0,0 +1,19 @@
+public class datatypes {
+    public static void main(String[] args) {
+        int a=2;
+        long b=2334;
+        float y=3.14f;
+        double z=4.5645634;
+        char r='e';
+        boolean g=true;
+
+        System.out.println("Integer:" +a);
+        System.out.println("Long:" +b);
+        System.out.println("Float: "+y);
+        System.out.println("Double: "+z);
+        System.out.println("Char: "+r);
+        System.out.println("Boolean: "+g);
+    
+
+    }
+}
