a6a0df1cdb54a7cc030153caae17b7e41e358770
PABOLU DURGA PRASAD <paboludurga2004@gmail.com>
2025-12-28 15:42:05 +0530

    Add files via upload

---
+++ Day-02/Pattern09.java
@@ -0,0 +1,32 @@
+public class Pattern09 {
+    static void center_triangle_and_inverse_triangle(int n){
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            for (int j=0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+            for (int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for (int i=0;i<n;i++){
+            for (int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for (int j=0;j<2*n-(2*i+
+            1);j++){
+                System.out.print("*");
+            }
+            for (int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        center_triangle_and_inverse_triangle(5);
+    }
+}
